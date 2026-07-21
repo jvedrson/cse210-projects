a6d3e0ef90ad8ebecb18edf4e4d20b5293c1fa02
Ederson Villalba <jvedrson@gmail.com>
2026-07-20 21:03:24 -0400

    W03 Team Activity: Scripture Memorizer Design

---
+++ week03/ScriptureMemorizer/Reference.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verseStart;
+    private int _verseEnd;
+
+    public Reference(string book, int chapter, int verseStart)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verseStart = verseStart;
+    }
+
+    public Reference(string book, int chapter, int verseStart, int verseEnd)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verseStart = verseStart;
+        _verseEnd = verseEnd;
+    }
+
+    public string GetDisplayText()
+    {
+        return null;
+    }
+}
---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+
+    public Scripture(Reference reference, string text)
+    {
+
+    }
+
+    public void HideRandomWords(int numbersToHide)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return null;
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return false;
+    }
+}
