de22666fe58046b5a103c7cefa61d38a408bdc21
Ederson Villalba <jvedrson@gmail.com>
2026-08-13 09:25:13 -0400

    W06 Team Activity: Eternal Quest Design

---
+++ week06/EternalQuest/EternalGoal.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class EternalGoal : Goal
+{
+    public EternalGoal(string name, string description, int points)
+        : base(name, description, points)
+    {
+    }
+
+    public override void RecordEvent()
+    {
+    }
+
+    public override bool IsComplete()
+    {
+        return false;
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return "";
+    }
+}
